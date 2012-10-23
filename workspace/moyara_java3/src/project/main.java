c900230d66f4eb626b256a716f3404d2f11306fb
moyara <moyara1@naver.com>
2012-10-23 15:21:28 +0900

    release note: http://goorm.org/community/notice?article_id=7

---
+++ workspace/moyara_java3/src/project/main.java
@@ -0,0 +1,14 @@
+package project;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test t = new test();
+		System.out.println(t.output());
+	}
+
+}
---
+++ workspace/moyara_java3/src/project/test.java
@@ -0,0 +1,8 @@
+package project;
+
+public class test{
+	public String output() {
+		int a= 0;
+		return "hello";
+	}
+}
