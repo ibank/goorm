5ae5a676e1999d274841a01f04eb9cc87cc2b928
Sung-tae Ryu <xenoz0718@gmail.com>
2012-10-24 01:21:09 +0900

    ignore list

--- ignore_git.sh
+++ ignore_git.sh
@@ -2,12 +2,12 @@
 
 echo "set ignore.list"
 
-if [ -f ~/.gitignore ]
+if [ -f ./.gitignore ]
 then
         echo "start setting the ignore list from ~/.gitignore for git"
         git config --global core.excludesfile ~/.gitignore
-        cat ~/.gitconfig
+        cat ./.gitconfig
 	echo "done"
 else
-        echo "~/.gitignore not exist"
+        echo "./.gitignore not exist"
 fi
